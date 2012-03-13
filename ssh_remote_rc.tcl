8455d07b85f331418566579eb76c2a07782c132d
Elio Grieco <grieco@egx.com>
2012-03-13 13:32:27 -0700

    Fixed the line skipping logic and added line trimming code.

--- ssh_remote_rc.tcl
+++ ssh_remote_rc.tcl
@@ -13,7 +13,6 @@ spawn ssh $remote_server
 expect "$remote_server%"
 
 # read rc file and execute its commands in the remote shell
-# :TODO: should look for blank or commented lines and skip them
 # :TODO: should look for "." or "source" lines which include other files and override those by reading them and sending their content
 if {[file readable "$rc_file_path"] == 1} {
 	puts "Reading configuration from: $rc_file_path"
@@ -22,9 +21,9 @@ if {[file readable "$rc_file_path"] == 1} {
 	close $rc_file
 	set rc_lines [split $rc_file_data "\n"]
 	foreach rc_line $rc_lines {
-		if {$rc_line == ""} {continue}
-		if [ regexp {\s#} $rc_line ] {continue}
-		puts $rc_line
+		if [regexp {^\s*$} $rc_line] {continue}
+		if [regexp {^\s*#} $rc_line] {continue}
+		puts [string trim $rc_line]
 	}
 } else {
 	puts "Cannot read remote RC file: $rc_file_path"
