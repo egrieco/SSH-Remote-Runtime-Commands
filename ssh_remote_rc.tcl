970e48cb09f5067267a2c1855562fcfe8555240f
Elio Grieco <grieco@egx.com>
2012-03-13 16:34:43 -0700

    Added a TODO comment and a temporary screen clear workaround to address it.

--- ssh_remote_rc.tcl
+++ ssh_remote_rc.tcl
@@ -14,6 +14,7 @@ expect "$remote_server%"
 
 # read rc file and execute its commands in the remote shell
 # :TODO: should look for "." or "source" lines which include other files and override those by reading them and sending their content
+# :TODO: should hide the output from ssh for cosmetic reasons. "log_user 0" and "stty -echo" do not seem to be the answer
 if {[file readable "$rc_file_path"] == 1} {
 	puts "Reading configuration from: $rc_file_path"
 
@@ -29,6 +30,8 @@ if {[file readable "$rc_file_path"] == 1} {
 		if [regexp {^\s*#} $rc_line] {continue}
 		send "[string trim $rc_line]\r"
 	}
+	# this is a temporary workaround until we can properly hide send data and output from ssh
+	send "clear\r"
 } else {
 	puts "Cannot read remote RC file: $rc_file_path"
 }
